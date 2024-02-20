036c0d786b47f287e79539e2e42138a883b7c762
Apiwat89 <apiwat89123@gamil.com>
2024-02-20 11:06:27 +0700

    e

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 mongoose.connect(
-    "mongodb://admin:QVNbpk99103@node57409-apiwattest.proen.app.ruk-com.cloud:11779",
+    "mongodb://admin:QVNbpk99103@node57409-apiwattest.proen.app.ruk-com.cloud/books",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
