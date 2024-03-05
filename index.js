2372b70af57ff4006b3cf3c6e1d755f6e60b698f
Apiwat89 <apiwat89123@gamil.com>
2024-03-05 09:45:02 +0700

    up

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 mongoose.connect(
     "mongodb://admin:QVNbpk99103@node57409-apiwattest.proen.app.ruk-com.cloud",
@@ -22,6 +23,7 @@ const Book = mongoose.model('book', {
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post("/books", async (req, res) => {
     try {
